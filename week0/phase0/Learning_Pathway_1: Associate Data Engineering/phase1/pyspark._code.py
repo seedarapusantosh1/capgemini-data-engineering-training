ec137e50f6ac4cb7083dd95353a9f131f2535495
seedarapusantosh1 <seedarapusantosh1@gmail.com>
2026-04-02 20:46:58 +0530

    Update pyspark._code.py

--- week0/phase0/Learning_Pathway_1: Associate Data Engineering/phase1/pyspark._code.py
+++ week0/phase0/Learning_Pathway_1: Associate Data Engineering/phase1/pyspark._code.py
@@ -1,1 +1,25 @@
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder.appName("Phase1").getOrCreate()
+
+customers = spark.createDataFrame([
+    (1, "Ravi", "Hyderabad", 25),
+    (2, "Sita", "Chennai", 32),
+    (3, "Arun", "Hyderabad", 28)
+], ["customer_id", "customer_name", "city", "age"])
+
+#Implementing the codes
+- Show all the customers**
+    customers.show()
+  
+- Show customers  from Chennai
+  customers.filter("city = 'Chennai'").show()
+
+- Show customers with age> 25
+  customers.filter(customers.age > 25).show()
+
+- Show inly customer_name and city
+   customers.select("customer_name", "city").show()
+
+- count customers city-wise
+   customers.groupBy("city").count().show()
