acf17b5cc23332176d27a836caca0af9e4597feb
seedarapusantosh1 <seedarapusantosh1@gmail.com>
2026-04-03 22:56:10 +0530

    Update pyspark_code.py

--- week0/phase3/pyspark_code.py
+++ week0/phase3/pyspark_code.py
@@ -9,13 +9,16 @@ sales=spark.read.format('csv').option('header','true').load('/samples/sales.csv'
 Codes:
 
 1. Read sales data -> clean nulls -> calculate daily sales
-    sales.groupBy("sale_date").agg(sum("total_amount").alias("daily_sales")).show()
+  daily_sales = sales.groupBy("sale_date").agg(sum("total_amount").alias("amount"))
+  display(daily_sales)
 
 2. Read customer data -> clean invalid rows -> city-wise revenue
-  customers.join(sales, "customer_id").groupBy("city").agg(sum("total_amount").alias("total_revenue")).show()
+  cus_amount=customers.join(sales, on="customer_id").groupBy(customers["city"]).agg(sum(sales["total_amount"]).alias("total_amount"))
+  display(cus_amount)
 
 3. Find repeat customers (>2 orders)
- customers.join(sales, "customer_id").groupBy("city").agg(sum("total_amount").alias("total_revenue")).show()
+  cus_amount= customers.join(sales, on="customer_id").groupBy(concat(col("first_name"),col("last_name")).alias("name")).agg(count(sales["sale_id"]).alias("total_orders")).filter(col("total_orders")>1)
+  display(cus_amount)
 
 4. Find highest spending customer in each city
   customer_totals = customers.join(sales, "customer_id")\
